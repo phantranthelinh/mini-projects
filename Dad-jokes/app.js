04a010ed162e8bb435f982e58488a6647a0b61e0
Phan Trần Thế Lĩnh <63492627+phantranthelinh@users.noreply.github.com>
2021-09-27 15:48:32 +0700

    dad-jokes

---
+++ Dad-jokes/app.js
@@ -0,0 +1,37 @@
+const jokeEl = document.querySelector('.joke')
+const jokeBtn = document.querySelector('#jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+generateJoke()
+
+// USING ASYNC / AWAIT
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+    const data = await res.json()
+    console.log(res)
+    jokeEl.innerText = data.joke
+}
+
+//USING .then()
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+
+//         },
+
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         .then(data => jokeEl.innerText = data.joke)
+//         .catch(error => console.error('Error: ', error))
+
+// }
